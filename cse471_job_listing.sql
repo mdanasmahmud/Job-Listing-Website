1b881a4d3f29ce12e024b6b93a73b5025e9e99e8
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2023-04-04 04:14:47 +0600

    ID: 20101149. Updated the mysql, added a token table for password management.

--- cse471_job_listing.sql
+++ cse471_job_listing.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 30, 2023 at 11:38 AM
+-- Generation Time: Apr 04, 2023 at 12:13 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -36,15 +36,17 @@ CREATE TABLE `company` (
   `company_details` text DEFAULT NULL,
   `account_type` varchar(100) NOT NULL,
   `nid_picture` varchar(100) DEFAULT NULL,
-  `account_creation_date` date NOT NULL
+  `account_creation_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `always_null` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `company`
 --
 
-INSERT INTO `company` (`id`, `name`, `email`, `password`, `company_logo`, `company_details`, `account_type`, `nid_picture`, `account_creation_date`) VALUES
-(1, 'Anas Mahmud Abid', 'abid@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'company', NULL, '0000-00-00');
+INSERT INTO `company` (`id`, `name`, `email`, `password`, `company_logo`, `company_details`, `account_type`, `nid_picture`, `account_creation_date`, `always_null`) VALUES
+(1, 'Bkash Company', 'abid@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'company', NULL, '0000-00-00 00:00:00', NULL),
+(2, 'alsolin', 'alsolin@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'company', NULL, '0000-00-00 00:00:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -59,18 +61,21 @@ CREATE TABLE `jobs_posted` (
   `job_details` text NOT NULL,
   `job_type` varchar(100) NOT NULL,
   `job_category` varchar(100) NOT NULL,
-  `job_salary` int(11) NOT NULL,
-  `job_location` varchar(100) NOT NULL,
+  `job_salary` int(100) NOT NULL,
   `job_creation_date` date NOT NULL,
-  `job_expiration_date` date NOT NULL
+  `job_expiration_date` date NOT NULL,
+  `job_post_status` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `jobs_posted`
 --
 
-INSERT INTO `jobs_posted` (`job_id`, `job_title`, `job_company_id`, `job_details`, `job_type`, `job_category`, `job_salary`, `job_location`, `job_creation_date`, `job_expiration_date`) VALUES
-(1, 'Bkash', 1, '0', 'Full Time', '0', 70000, '0', '0000-00-00', '0000-00-00');
+INSERT INTO `jobs_posted` (`job_id`, `job_title`, `job_company_id`, `job_details`, `job_type`, `job_category`, `job_salary`, `job_creation_date`, `job_expiration_date`, `job_post_status`) VALUES
+(2, 'Pathao Manager', 2, 'This is for the business people', 'Part Time', 'Manager', 25000, '2023-04-01', '2023-04-01', 'Approved'),
+(3, 'Foodpanda Manager', 1, 'This is for the business people', 'Full Time', 'Manager', 30000, '2023-04-01', '2023-04-01', 'Approved'),
+(4, 'Google Manager', 2, 'This is for the business people', 'Part Time', 'Manager', 60000, '2023-04-01', '2023-04-01', 'Approved'),
+(5, 'Tesla Data Entry', 2, 'This is for the business people', 'Part Time', 'Data Entry', 70000, '2023-04-01', '2023-04-08', 'Pending');
 
 -- --------------------------------------------------------
 
@@ -89,15 +94,18 @@ CREATE TABLE `job_seeker` (
   `nid_picture` varchar(100) DEFAULT NULL,
   `account_creation_date` date NOT NULL,
   `bookmarked_company` varchar(100) DEFAULT NULL,
-  `account_type` varchar(100) NOT NULL
+  `account_type` varchar(100) NOT NULL,
+  `always_null` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `job_seeker`
 --
 
-INSERT INTO `job_seeker` (`id`, `name`, `email`, `password`, `picture`, `portfolio`, `security_answer`, `nid_picture`, `account_creation_date`, `bookmarked_company`, `account_type`) VALUES
-(1, 'Anas Mahmud Abid', 'abid@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'c988fa7c33ce43962b9803702b747a35', NULL, '0000-00-00', NULL, 'job_seeker');
+INSERT INTO `job_seeker` (`id`, `name`, `email`, `password`, `picture`, `portfolio`, `security_answer`, `nid_picture`, `account_creation_date`, `bookmarked_company`, `account_type`, `always_null`) VALUES
+(1, 'Anas Mahmud Abid', 'abid@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'c988fa7c33ce43962b9803702b747a35', NULL, '0000-00-00', NULL, 'job_seeker', NULL),
+(4, 'admin', 'admin@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'c988fa7c33ce43962b9803702b747a35', NULL, '0000-00-00', NULL, 'admin', NULL),
+(5, 'Anas Mahmud Abid Fake', 'abidf@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', NULL, NULL, 'c988fa7c33ce43962b9803702b747a35', NULL, '0000-00-00', NULL, 'job_seeker', NULL);
 
 -- --------------------------------------------------------
 
@@ -107,11 +115,39 @@ INSERT INTO `job_seeker` (`id`, `name`, `email`, `password`, `picture`, `portfol
 
 CREATE TABLE `message` (
   `message_id` int(100) NOT NULL,
-  `message_box_id` int(100) NOT NULL,
   `message_sender_id` int(100) NOT NULL,
   `message_receiver_id` int(100) NOT NULL,
   `message_detail` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`message_id`, `message_sender_id`, `message_receiver_id`, `message_detail`) VALUES
+(1, 1, 3, 'Hey there welcome to our company. We hope you can start by monday.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `new_table`
+--
+
+CREATE TABLE `new_table` (
+  `job_s_id` int(100) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `new_table`
+--
+
+INSERT INTO `new_table` (`job_s_id`) VALUES
+(1),
+(1),
+(4),
+(4),
+(5),
+(5);
 
 -- --------------------------------------------------------
 
@@ -125,6 +161,21 @@ CREATE TABLE `notification` (
   `notification_details` varchar(100) NOT NULL,
   `notification_link` varchar(100) NOT NULL,
   `notification_time` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `id` int(100) NOT NULL,
+  `user_id` int(100) NOT NULL,
+  `token` varchar(128) NOT NULL,
+  `created_at` date NOT NULL,
+  `expires_at` date NOT NULL,
+  `used` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -162,6 +213,12 @@ ALTER TABLE `notification`
   ADD PRIMARY KEY (`notification_id`);
 
 --
+-- Indexes for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -169,31 +226,37 @@ ALTER TABLE `notification`
 -- AUTO_INCREMENT for table `company`
 --
 ALTER TABLE `company`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `jobs_posted`
 --
 ALTER TABLE `jobs_posted`
-  MODIFY `job_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `job_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `job_seeker`
 --
 ALTER TABLE `job_seeker`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `message_id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `message_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `notification`
 --
 ALTER TABLE `notification`
   MODIFY `notification_id` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
