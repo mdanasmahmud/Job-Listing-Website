2a03aa507b4ff436e838812183f74ca3178a9a17
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2023-04-19 06:37:25 +0600

    ID: 20101149, Added the functionality to search any user by the company, will implement the message system later

--- company_search.php
+++ company_search.php
@@ -34,21 +34,52 @@ if($account_type != 'company'){
 
 <!-- courses section starts  -->
 
-<section class="courses">
+<section class="search-form">
+<h1 class="heading">Search Any User</h1>
+   <form action="" method="post">
+      <input type="text" name="search" placeholder="search jobs, companies..." class="box">
+      <input type="submit" name="submit" value="search" class="btn">
+   </form>
+</section>
 
-   <h1 class="heading">Search Result</h1>
+<section class="courses">
 
    <div class="box-container">
 
       
+      
+
+      <?php
+         if(isset($_POST['submit'])){
+            $search_item = $_POST['search'];
+            $select_jobs = mysqli_query($conn, "SELECT * FROM `job_seeker` WHERE name LIKE '%{$search_item}%'") or die('query failed');
+            if(mysqli_num_rows($select_jobs) > 0){
+            while($fetch_job = mysqli_fetch_assoc($select_jobs)){
+      ?>
+
       <div class="box">
          <div class="tutor">
             <div>
-               <h3>Search Aything</h3>
-               <span></span>
+               <h3>Name: <?= $fetch_job['name']; ?></h3>
+               <span>Email: <?= $fetch_job['email']; ?></span>
             </div>
          </div>
       </div>
+
+
+      <form action="" method="post" class="box" hidden>
+         <div class="price"><?php echo $fetch_job['name']; ?></div>
+         <div class="name"><?php echo $fetch_job['email']; ?></div>
+      </form>
+      <?php
+               }
+            }else{
+               echo '<p class="empty">Unable To Find The User</p>';
+            }
+         }else{
+            echo '<p class="empty">Search Any User</p>';
+         }
+      ?>
 
    </div>
 
